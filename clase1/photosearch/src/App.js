945b89a84b7b7c355d8b5bd88b909c69a252ff10
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2023-06-06 23:28:56 +0200

    Cabecera

---
+++ clase1/photosearch/src/App.js
@@ -0,0 +1,17 @@
+
+import Buscador from "./components/Buscador";
+
+function App() {
+
+  return (
+    <div className="app container">
+      <div className="jumbotron">
+        <p className="lead text center">Buscador de Imágenes</p>
+
+        <Buscador />
+      </div>
+    </div>
+  );
+}
+
+export default App;
