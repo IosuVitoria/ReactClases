004aaf9dade1b9baf336198e96bc1680530def45
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2023-06-07 12:59:33 +0200

    CV exercise

---
+++ clase2/CV/src/components/languages/Language.jsx
@@ -0,0 +1,13 @@
+const languages = ({ languages, wrlevel, splevel}) => {
+    return (
+      <div>
+        <h3>{languages}</h3>
+        <p>Writing Level: {wrlevel}</p>
+        <p>Speaking Level: {splevel}</p>
+        
+
+      </div>
+    );
+  };
+
+export default languages
