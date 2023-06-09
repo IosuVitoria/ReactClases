c451cdf768f98f9b9ca9fbcea41bfe2848d714b1
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2023-06-09 16:34:43 +0200

    detail page completed

--- clase4/dayana01/src/Components/Detail.jsx
+++ clase4/dayana01/src/Components/Detail.jsx
@@ -1,7 +1,30 @@
-const Detail = () => {
-  return (
-    <div>ESte el componente del detalle</div>
-  )
-}
+import { useParams } from 'react-router-dom';
+import data from '../Data/product.json';
 
-export default Detail
+const Detail = () => {
+  const { id } = useParams();
+  console.log(id);
+
+  const findProduct = data.items.find((product) => product.id === id);
+
+  if (!findProduct) {
+    return <div>Producto no encontrado.</div>;
+  }
+
+  return (
+    <div>
+      <h2>Detalle del producto</h2>
+      <img src={findProduct.imageUrl} alt="" />
+      <h3>{findProduct.name}</h3>
+      <h3>{findProduct.price}</h3>
+      <ul>
+        {findProduct.sizes.map((size) => (
+          <li key={size}>{size}</li>
+        ))}
+      </ul>
+      <p>{findProduct.description}</p>
+    </div>
+  );
+};
+
+export default Detail;
